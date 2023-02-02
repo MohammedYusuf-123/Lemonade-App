93daca4e245089876a87143c55a3d4343c20692f
mdyus <mohammedyusufmujawar92@gmail.com>
2023-02-02 21:27:30 +0530

    One traverse through images completed.

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.lemonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    LemonadeInteractive()
                 }
             }
         }
@@ -30,14 +40,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun LemonadeInteractive() {
+    var imageCounter by remember { mutableStateOf(1) }
+    var imageSource = when (imageCounter) {
+        1 -> R.drawable.lemon_tree
+        2 -> R.drawable.lemon_squeeze
+        3 -> R.drawable.lemon_drink
+        4 -> R.drawable.lemon_restart
+        else -> R.drawable.lemon_tree
+    }
+    var textSource = when (imageCounter) {
+        1 -> R.string.screen1_text
+        2 -> R.string.screen2_text
+        3 -> R.string.screen3_text
+        4 -> R.string.screen4_text
+        else -> R.string.screen1_text
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(text = stringResource(textSource), fontSize = 18.sp)
+        Spacer(modifier = Modifier.height(16.dp))
+        Image(
+            painter = painterResource(imageSource),
+            contentDescription = null,
+            modifier = Modifier
+                .border(
+                    3.dp,
+                    color = Color(105, 205, 216),
+                    shape = RoundedCornerShape(2.dp)
+                )
+                .clickable { imageCounter++ }
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun LemonadePreview() {
     LemonadeTheme {
-        Greeting("Android")
+        LemonadeInteractive()
     }
 }
